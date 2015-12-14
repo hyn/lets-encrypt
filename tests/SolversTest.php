7fd2c67634c772f2e457fed307dc69d7e2b5e0c2
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-12-14 16:13:52 +0100

    test to assert existence of default solvers

---
+++ tests/SolversTest.php
@@ -0,0 +1,11 @@
+<?php namespace Hyn\LetsEncrypt\Tests;
+
+use Hyn\LetsEncrypt\Resources\Challenge;
+
+class SolversTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDefaultSolversExist()
+    {
+        $this->assertTrue(in_array('Hyn\LetsEncrypt\Solvers', Challenge::getSolverLocations()));
+    }
+}
