39428871907384066e4dc0705d9d0d3d232fd21c
Daniel Klabbers <daniel+hyn@klabbers.email>
2016-03-09 09:18:29 +0100

    added phpseclib for Crypt_RSA replacement

--- src/Helpers/KeyPairGenerator.php
+++ src/Helpers/KeyPairGenerator.php
@@ -2,14 +2,14 @@
 
 namespace Hyn\LetsEncrypt\Helpers;
 
-use Crypt_RSA;
+use phpseclib\Crypt\RSA;
 use Kelunik\Acme\KeyPair;
 
 abstract class KeyPairGenerator
 {
     public static function generate()
     {
-        $keys = (new Crypt_RSA())->createKey(4096);
+        $keys = (new RSA())->createKey(4096);
 
         return new KeyPair($keys['privatekey'], $keys['publickey']);
     }
