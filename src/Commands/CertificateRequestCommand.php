dccdc2cedb62cc315b716b2675b8cfe2bd5fe625
Daniel Klabbers <daniel+hyn@klabbers.email>
2016-03-10 16:40:15 +0100

    Simple implementation done, continue to testing.

--- src/Commands/CertificateRequestCommand.php
+++ src/Commands/CertificateRequestCommand.php
@@ -2,13 +2,15 @@
 
 namespace Hyn\LetsEncrypt\Commands;
 
+use Hyn\LetsEncrypt\Solvers\Http01Solver;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class CertificateRequestCommand extends Command {
+class CertificateRequestCommand extends Command
+{
 
     /**
      * {@inheritdoc}
@@ -16,14 +18,39 @@ class CertificateRequestCommand extends Command {
     protected function configure()
     {
         $this->setName('certificate:request')
-            ->addArgument('hostnames', InputArgument::IS_ARRAY, 'Specify the hostnames you want to request a certificate for [example.com www.example.com ..].')
-            ->addOption('http', false, InputOption::VALUE_OPTIONAL, 'Specify the public directory for this domain, solves the verification using file placement.')
-            ->addOption('dns', false, InputOption::VALUE_OPTIONAL, 'Set to true, to solve verification using DNS, will wait for you to set the record.');
+            ->addArgument('hostnames', InputArgument::IS_ARRAY,
+                'Specify the hostnames you want to request a certificate for [example.com www.example.com ..].')
+            ->addOption('http', false, InputOption::VALUE_OPTIONAL,
+                'Specify the public directory for this domain, solves the verification using file placement.', false)
+            ->addOption('dns', false, InputOption::VALUE_OPTIONAL,
+                'Set to true, to solve verification using DNS, will wait for you to set the record.', false);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-
+        if ($input->getOption('http')) {
+            return $this->runHttpMethod($input, $output);
+        } elseif ($input->getOption('dns')) {
+            // todo
+        }
     }
 
+    protected function runHttpMethod(InputInterface $input, OutputInterface $output)
+    {
+        $public_path = rtrim($input->getOption('http'), '/');
+        $challenge_path = "{$public_path}/.well-known/acme-challenge/";
+
+        if (!is_dir($challenge_path)) {
+            if( !mkdir($challenge_path, 0755, true)) {
+                $output->writeln('<error>Unable to generate the acme challenge directory in the specified public directory, manually create path or set ownership to: ' . $challenge_path);
+                return;
+            }
+        }
+
+        // Set the challenge path.
+        Http01Solver::setChallengePublicPath($challenge_path);
+
+
+
+    }
 }
--- src/Solvers/Http01Solver.php
+++ src/Solvers/Http01Solver.php
@@ -19,6 +19,43 @@ use Hyn\LetsEncrypt\Resources\Challenge;
 class Http01Solver implements ChallengeSolverContract
 {
     /**
+     * Path to store the challenge file.
+     * @var string
+     */
+    protected static $challenge_public_path;
+    /**
+     * @var Challenge
+     */
+    protected $challenge;
+
+    /**
+     * @var \stdClass
+     */
+    protected $payload;
+
+    /**
+     * Unique token for the certificate request.
+     *
+     * @var string
+     */
+    protected $token;
+
+    /**
+     * Sets the publicly available directory to store challenge files into.
+     *
+     * @param string $challenge_public_path
+     * @throws \Exception
+     */
+    public static function setChallengePublicPath($challenge_public_path)
+    {
+        if (is_dir($challenge_public_path)) {
+            self::$challenge_public_path = rtrim($challenge_public_path, '/');
+        } else {
+            throw new \Exception("Directory to write challenge files into, must exist.");
+        }
+    }
+
+    /**
      * Solves a certain challenge.
      *
      * Return false if not possible.
@@ -30,6 +67,31 @@ class Http01Solver implements ChallengeSolverContract
      */
     public function solve(Challenge $challenge, $payload)
     {
-        // TODO: Implement solve() method.
+        $this->challenge = $challenge;
+        $this->payload = $payload;
+        $this->token = $payload->token;
+
+        $httpFeedback = $this->acme()->generateHttp01Payload($this->token);
+
+        if (file_put_contents(static::$challenge_public_path . '/' . $this->token, $httpFeedback) === false) {
+            return false;
+        }
+
+        $this->acme()->selfVerify($challenge->getHostname(), $this->token, $httpFeedback);
+        $this->acme()->answerChallenge($payload->uri, $httpFeedback);
+
+        $this->acme()->pollForChallenge($challenge->getLocation());
+
+        unlink(static::$challenge_public_path . '/' . $this->token);
+
+        return true;
+    }
+
+    /**
+     * @return \Hyn\LetsEncrypt\Acme\Client
+     */
+    protected function acme()
+    {
+        return $this->challenge->getCertificate()->getAccount()->acme();
     }
 }
