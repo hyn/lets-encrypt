83c1e7ec3997141deeeac54d226a69b009f48f9f
Luceos <daniel+github@klabbers.email>
2016-03-10 07:36:27 -0500

    Applied fixes from StyleCI

--- src/Commands/CertificateRequestCommand.php
+++ src/Commands/CertificateRequestCommand.php
@@ -6,8 +6,8 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
-class CertificateRequestCommand extends Command {
-
+class CertificateRequestCommand extends Command
+{
     /**
      * {@inheritdoc}
      */
