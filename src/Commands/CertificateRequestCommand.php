fce30f5291d5138101bd7de85f50bf66323f0ced
Daniel Klabbers <daniel+hyn@klabbers.email>
2016-03-10 16:49:25 +0100

    Seems to be working already. Should be tested in an environment publicly available with a resolving hostname.

--- src/Commands/CertificateRequestCommand.php
+++ src/Commands/CertificateRequestCommand.php
@@ -2,6 +2,9 @@
 
 namespace Hyn\LetsEncrypt\Commands;
 
+use Hyn\LetsEncrypt\Helpers\Configured;
+use Hyn\LetsEncrypt\Resources\Account;
+use Hyn\LetsEncrypt\Resources\Certificate;
 use Hyn\LetsEncrypt\Solvers\Http01Solver;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -11,7 +14,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class CertificateRequestCommand extends Command
 {
-
+    use Configured;
     /**
      * {@inheritdoc}
      */
@@ -20,6 +23,10 @@ class CertificateRequestCommand extends Command
         $this->setName('certificate:request')
             ->addArgument('hostnames', InputArgument::IS_ARRAY,
                 'Specify the hostnames you want to request a certificate for [example.com www.example.com ..].')
+            ->addOption('account', 'a', InputOption::VALUE_REQUIRED,
+                'Lets Encrypt account name, does not have to exist.')
+            ->addOption('email', 'e', InputOption::VALUE_REQUIRED,
+                'Lets Encrypt email address.')
             ->addOption('http', false, InputOption::VALUE_OPTIONAL,
                 'Specify the public directory for this domain, solves the verification using file placement.', false)
             ->addOption('dns', false, InputOption::VALUE_OPTIONAL,
@@ -28,6 +35,11 @@ class CertificateRequestCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        if (!count($input->getArgument('hostnames'))) {
+            $output->writeln('<error>You need to specify valid hostnames!</error>');
+            return;
+        }
+
         if ($input->getOption('http')) {
             return $this->runHttpMethod($input, $output);
         } elseif ($input->getOption('dns')) {
@@ -50,7 +62,11 @@ class CertificateRequestCommand extends Command
         // Set the challenge path.
         Http01Solver::setChallengePublicPath($challenge_path);
 
-
-
+        $account = new Account($input->getOption('account'), $input->getOption('email'));
+        $certificate = new Certificate($account);
+        foreach($input->getArgument('hostnames') as $hostname) {
+            $certificate->addHostname($hostname);
+        }
+        dd($certificate->request());
     }
 }
